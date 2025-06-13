f3fe6b2729b6f6f5650b3de19d4093cae1295daf
Ayush kumar <119615444+Ayush8566@users.noreply.github.com>
2025-06-14 00:17:33 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter,Routes,Route }   from 'react-router-dom';
 import React from 'react'
 import Home from './pages/Home';
-import Signin from './pages/Signin';
+import Signin from './pages/SignIn';
 
 import SingnUp from './pages/SignUp';
 import About from './pages/About';
